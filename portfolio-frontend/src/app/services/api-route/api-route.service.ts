dbf16d6008e83c8fb07445dcee93a69d12873a0f
Sebastian Horacio Charras <sebastiancharras@outlook.com>
2023-02-23 17:02:25 -0300

    Modificada ruta api

--- portfolio-frontend/src/app/services/api-route/api-route.service.ts
+++ portfolio-frontend/src/app/services/api-route/api-route.service.ts
@@ -4,7 +4,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class ApiRouteService {
-  private _route: string = 'http://api.sebastiancharras.com:8080/api/';
+  private _route: string = 'http://82.180.131.56:8080/api/';
 
   constructor() {}
 
