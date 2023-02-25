d8f40b1212468910b5f224011df0122c544d8ea8
= <=>
2023-02-25 22:19:47 +0000

    Actualizada ruta api

--- portfolio-frontend/src/app/services/api-route/api-route.service.ts
+++ portfolio-frontend/src/app/services/api-route/api-route.service.ts
@@ -4,7 +4,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class ApiRouteService {
-  private _route: string = 'http://82.180.131.56:8080/api/';
+  private _route: string = 'https://api.sebastiancharras.com:8081/api/';
 
   constructor() {}
 
