5f308d196d082d217a629a5834aa9e39bd555a28
Sebastian Horacio Charras <sebastiancharras@outlook.com>
2023-03-03 13:57:52 -0300

    modificada ruta api

--- portfolio-frontend/src/app/services/api-route/api-route.service.ts
+++ portfolio-frontend/src/app/services/api-route/api-route.service.ts
@@ -4,7 +4,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class ApiRouteService {
-  private _route: string = 'https://portfolio-argentina-programa.fly.dev//api/';
+  private _route: string = 'https://portfolio-argentina-programa.fly.dev/api/';
 
   constructor() {}
 
